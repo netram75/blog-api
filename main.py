cb2561e08f79ec3ba04b899c31baec110c12d25f
netram75 <netram.24bcs10329@sst.scaler.com>
2026-05-21 23:00:46 +0530

    day2: PUT DELETE + validation + error handling

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, EmailStr, Field
 
 # this is the main application instance
 app = FastAPI()
 
+class User(BaseModel):
+    id: int
+    name: str = Field(..., min_length=2, max_length=50)
+    role: str
+    email: EmailStr
+
+
 # a simple python dictionary to act as our database for today
 fake_user_db = {
-    1: {"id":1, "name":"Alice", "role": "Admin"},
-    2: {"id":2, "name":"bob", "role":"Author"},
-    42: {"id":42, "name":"netram","role": "super user"}
+    1: {"id": 1, "name": "Alice", "role": "Admin", "email": "alice@example.com"},
+    2: {"id": 2, "name": "Bob", "role": "Author", "email": "bob@example.com"},
+    42: {"id": 42, "name": "Netram", "role": "Super User", "email": "netram@example.com"}
 }
 
 # 1. The root endpoint
@@ -26,3 +34,21 @@ def get_single_user(user_id: int):
     if user_id not in fake_user_db:
         raise HTTPException(status_code=404, detail="User not found")
     return fake_user_db[user_id]
+
+@app.put("/users/{user_id}")
+def update_user(user_id: int, updated_user: User):
+    if user_id not in fake_user_db:
+        raise HTTPException(status_code=404, detail="User Not Found")
+    
+    #save the updated data back into our fake database
+    fake_user_db[user_id] = updated_user.model_dump()
+    return {"message":"User updated Successfully", "user": updated_user}
+
+@app.delete("/user/{user_id}")
+def delete_user(user_id: int):
+    if user_id not in fake_user_db:
+        raise HTTPException(status_code=404, detail="User not found")
+    
+    # remove the user from our fake database
+    del fake_user_db[user_id]
+    return {"message": f"user {user_id} deleted successfully"}
